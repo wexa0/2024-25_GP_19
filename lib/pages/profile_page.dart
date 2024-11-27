b1e018372694aea04e053b213912717b0cc4eabf
Tarfah-Amin <tarfah.ateeq@hotmail.com>
2024-11-27 19:15:40 +0300

    #update profile

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -79,7 +79,7 @@ void _refreshUserData() {
         centerTitle: true,
         automaticallyImplyLeading: false,
       ),
-      backgroundColor: Color(0xFFF5F5F5),
+      backgroundColor:  Color(0xFFF5F7F8),
       body: SafeArea(
         child: isLoading
              ? Center(
@@ -109,44 +109,69 @@ void _refreshUserData() {
                       children: [
                         Container(
                           width: double.infinity,
-                          color: const Color.fromARGB(255, 226, 231, 234),
+                          color:  Color(0xFFF5F7F8),
                           padding: EdgeInsets.all(16),
                           child: Padding(
                             padding: const EdgeInsets.symmetric(horizontal: 20),
                             child: Text(
                               'Profile Information',
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
+                             style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontFamily: 'Poppins',
+                  fontSize: 18,
+                  color: Color(0xFF545454),
+                ),
+                            ),
+                          ),
+                        ),Padding(
+                          padding: EdgeInsets.only(right:16, left: 16), 
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(16),  
+                              topRight: Radius.circular(16), 
+                            ),
+                            child: Material(
+                              color: Colors.white,
+                              child: InkWell(
+                                onTap: () => user.showEditDialog(context, 'name', _refreshUserData),
+                                child: ListTile(
+                                  title: Text('Name',          style: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
+               color: Color(0xFF545454))),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Text('${user.firstName ?? ''} ${user.lastName ?? ''}'.trim()),
+                                      SizedBox(width: 8),
+                                      Icon(Icons.arrow_forward_ios),
+                                    ],
+                                  ),
+                                ),
                               ),
                             ),
                           ),
                         ),
-                        Material(
-                          color: Colors.white,
-                          child: InkWell(
-                            onTap: () => user.showEditDialog(context, 'name', _refreshUserData),
-                            child: ListTile(
-                              title: Text('Name'),
-                              trailing: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Text(
-                                      '${user.firstName ?? ''} ${user.lastName ?? ''}'
-                                          .trim()),
-                                  SizedBox(width: 8),
-                                  Icon(Icons.arrow_forward_ios),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
+                                              Divider(
+                        color:Color(0xFF104A73),          // Set the color of the divider
+                        thickness: 0.5,                // Set the thickness of the divider
+                        indent: 30,                  // Set the indent on the left
+                        endIndent: 30,               // Set the indent on the right
+                         height: 0,   
+                      ), Padding(
+  padding: EdgeInsets.only(right:16, left: 16), 
+  child:
                         Material(
                           color: Colors.white,
                           child: InkWell(
                             onTap: () => user.showEditDialog(context, 'email', _refreshUserData),
                             child: ListTile(
-                              title: Text('Email'),
+                              title: Text('Email',          style: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
+               color: Color(0xFF545454))),
                               trailing: Row(
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
@@ -157,13 +182,26 @@ void _refreshUserData() {
                               ),
                             ),
                           ),
-                        ),
+                        )),
+                         Divider(
+                        color:Color(0xFF104A73),          // Set the color of the divider
+                        thickness: 0.5,                // Set the thickness of the divider
+                        indent: 30,                  // Set the indent on the left
+                        endIndent: 30,               // Set the indent on the right
+                         height: 0,   
+                      ), Padding(
+  padding: EdgeInsets.only(right:16, left: 16), 
+  child:
                         Material(
                           color: Colors.white,
                           child: InkWell(
                                 onTap: () => user.showEditDialog(context, 'dateOfBirth', _refreshUserData),
                             child: ListTile(
-                              title: Text('Date of Birth'),
+                              title: Text('Date of Birth' ,   style: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
+               color: Color(0xFF545454))),
                               trailing: Row(
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
@@ -174,13 +212,32 @@ void _refreshUserData() {
                               ),
                             ),
                           ),
-                        ),
+                        )),
+                         Divider(
+                        color:Color(0xFF104A73),          // Set the color of the divider
+                        thickness: 0.5,                // Set the thickness of the divider
+                        indent: 30,                  // Set the indent on the left
+                        endIndent: 30,               // Set the indent on the right
+                         height: 0,   
+                      ), Padding(
+  padding: EdgeInsets.only(right:16, left: 16), 
+  child: ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(16),  
+                              bottomRight: Radius.circular(16), 
+                            ),
+                            child:
                         Material(
                           color: Colors.white,
                           child: InkWell(
                             onTap: () => user.showChangePasswordDialog(context),
                             child: ListTile(
-                              title: Text('Change Password'),
+                              title: Text('Change Password', 
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
+               color: Color(0xFF545454))),
                               trailing: Row(
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
@@ -191,10 +248,10 @@ void _refreshUserData() {
                               ),
                             ),
                           ),
-                        ),
+                        ))),
                         Container(
                           width: double.infinity,
-                          color: const Color.fromARGB(255, 226, 231, 234),
+                          color:   Color(0xFFF5F7F8),
                           padding: EdgeInsets.all(16),
                           child: Padding(
                             padding: const EdgeInsets.symmetric(horizontal: 20),
@@ -203,10 +260,21 @@ void _refreshUserData() {
                               style: TextStyle(
                                 fontSize: 18,
                                 fontWeight: FontWeight.bold,
+                                 color: Color(0xFF545454)
                               ),
                             ),
                           ),
                         ),
+                        Padding( padding: EdgeInsets.only(right:16, left: 16), 
+                          child:
+                         ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(16),  
+                              topRight: Radius.circular(16),
+                               bottomLeft: Radius.circular(16),  
+                              bottomRight: Radius.circular(16), 
+                            ),
+                            child:
                         Material(
                           color: Colors.white,
                           child: InkWell(
@@ -219,7 +287,12 @@ void _refreshUserData() {
                               launch(emailLaunchUri.toString());
                             },
                             child: ListTile(
-                              title: Text('Contact us'),
+                              title: Text('Contact us',
+            style: TextStyle(
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.w600,
+              fontSize: 14,
+               color: Color(0xFF545454)),),
                               trailing: Row(
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
@@ -230,7 +303,7 @@ void _refreshUserData() {
                               ),
                             ),
                           ),
-                        ),
+                         ) )),
                         SizedBox(height: 20),
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 20),
