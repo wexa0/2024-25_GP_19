87b455308afbda789d686fa7a19cad6ad8f79efc
wexa0 <443200416@student.ksu.edu.sa>
2025-04-18 19:06:41 +0300

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ void main() async {
 
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown, // اختياري، لو تبي يقبل رأسي مقلوب
+    DeviceOrientation.portraitDown, 
   ]);
 
 
